312d469401e89b618a3a04eb5a055718d349fba8
brendonparker <brendon.parker@rfsmart.com>
2022-07-25 19:45:00 -0400

    Add DemoApp2 which uses more modern dotnet stack/api

---
+++ src/CromulentBisgetti.DemoApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using CromulentBisgetti.ContainerPacking;
+using CromulentBisgetti.ContainerPacking.Entities;
+using Microsoft.AspNetCore.Http.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<JsonOptions>(opts =>
+{
+    // Use PascalCase
+    opts.SerializerOptions.PropertyNamingPolicy = null;
+});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapPost("/api/containerpacking", async (HttpContext context) =>
+{
+    var req = await context.Request.ReadFromJsonAsync<ContainerPackingRequest>();
+    return PackingService.Pack(req.Containers, req.ItemsToPack, req.AlgorithmTypeIDs);
+});
+
+app.Run();
+
+public record ContainerPackingRequest(List<Container> Containers, List<Item> ItemsToPack, List<int> AlgorithmTypeIDs);
